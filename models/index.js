2fcc433b738e8c3f99aed26630630756d711183c
Daniel Perrelly <danperrelly@gmail.com>
2015-08-11 17:58:44 -0400

    finished first day of work

---
+++ models/index.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/wikistack');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+
+var pageSchema = new mongoose.Schema({
+  title:    {type: String, required: true},
+  urlTitle: {type: String, required: true},
+  content:  {type: String, required: true},
+  status:   {type: String, enum: ['open', 'closed']},
+  date:     {type: Date, default: Date.now},
+  author:   {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+});
+
+var userSchema = new mongoose.Schema({
+  name: {first: {type: String, required: true}, last: {type: String, required: true}},
+  email: {type: String, unique: true, required: true}
+});
+
+var Page = mongoose.model('Page', pageSchema);
+var User = mongoose.model('User', userSchema);
+
+module.exports = {
+  Page: Page,
+  User: User
+};
